c72cc8b6d7c4b5df5f3c618ea754e2be3aed6070
Suyash Saxena <35092213+Suyash1266@users.noreply.github.com>
2024-03-14 15:27:05 +0530

    Update setup.py
    - Added py_modules
    - Removed find_packages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="scrappers",
     version="0.1.0",
-    packages=find_packages(),
     install_requires=[
         "selenium>=4.18.1",
         "beautifulsoup4>=4.12.3",
     ],
     python_requires=">=3.10",
     package_dir={'scrappers': 'src'},
+    py_modules=['scrappers.amazon', 'scrappers.googlemaps', 'scrappers.exceptions']
 )
